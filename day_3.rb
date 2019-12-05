f56f12c185467a355d368424c807441ed4cd23c1
jenroper <jen.roper@gmail.com>
2019-12-05 00:35:02 -0500

    wip

--- day_3.rb
+++ day_3.rb
@@ -40,6 +40,10 @@ def get_lines(input)
   return lines
 end
 
+def get_intersections(lines_1, lines_2) {
+  # returns array of x,y coordinates
+}
+
 wire_1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
 wire_2 = "U62,R66,U55,R34,D71,R55,D58,R83"
 
